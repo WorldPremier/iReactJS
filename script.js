0556db58ab9df02b2d9db9dcd15898afe43e96a4
Franklyn Reyes <franklynreyes777@yahoo.com>
2024-04-27 01:10:07 -0400

    coding with react

---
+++ script.js
@@ -0,0 +1,230 @@
+const data = [
+  {
+    id: 1,
+    title: "The Lord of the Rings",
+    publicationDate: "1954-07-29",
+    author: "J. R. R. Tolkien",
+    genres: [
+      "fantasy",
+      "high-fantasy",
+      "adventure",
+      "fiction",
+      "novels",
+      "literature",
+    ],
+    hasMovieAdaptation: true,
+    pages: 1216,
+    translations: {
+      spanish: "El señor de los anillos",
+      chinese: "魔戒",
+      french: "Le Seigneur des anneaux",
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.52,
+        ratingsCount: 630994,
+        reviewsCount: 13417,
+      },
+      librarything: {
+        rating: 4.53,
+        ratingsCount: 47166,
+        reviewsCount: 452,
+      },
+    },
+  },
+  {
+    id: 2,
+    title: "The Cyberiad",
+    publicationDate: "1965-01-01",
+    author: "Stanislaw Lem",
+    genres: [
+      "science fiction",
+      "humor",
+      "speculative fiction",
+      "short stories",
+      "fantasy",
+    ],
+    hasMovieAdaptation: false,
+    pages: 295,
+    translations: {},
+    reviews: {
+      goodreads: {
+        rating: 4.16,
+        ratingsCount: 11663,
+        reviewsCount: 812,
+      },
+      librarything: {
+        rating: 4.13,
+        ratingsCount: 2434,
+        reviewsCount: 0,
+      },
+    },
+  },
+  {
+    id: 3,
+    title: "Dune",
+    publicationDate: "1965-01-01",
+    author: "Frank Herbert",
+    genres: ["science fiction", "novel", "adventure"],
+    hasMovieAdaptation: true,
+    pages: 658,
+    translations: {
+      spanish: "",
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.25,
+        ratingsCount: 1142893,
+        reviewsCount: 49701,
+      },
+    },
+  },
+  {
+    id: 4,
+    title: "Harry Potter and the Philosopher's Stone",
+    publicationDate: "1997-06-26",
+    author: "J. K. Rowling",
+    genres: ["fantasy", "adventure"],
+    hasMovieAdaptation: true,
+    pages: 223,
+    translations: {
+      spanish: "Harry Potter y la piedra filosofal",
+      korean: "해리 포터와 마법사의 돌",
+      bengali: "হ্যারি পটার এন্ড দ্য ফিলোসফার্স স্টোন",
+      portuguese: "Harry Potter e a Pedra Filosofal",
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.47,
+        ratingsCount: 8910059,
+        reviewsCount: 140625,
+      },
+      librarything: {
+        rating: 4.29,
+        ratingsCount: 120941,
+        reviewsCount: 1960,
+      },
+    },
+  },
+  {
+    id: 5,
+    title: "A Game of Thrones",
+    publicationDate: "1996-08-01",
+    author: "George R. R. Martin",
+    genres: ["fantasy", "high-fantasy", "novel", "fantasy fiction"],
+    hasMovieAdaptation: true,
+    pages: 835,
+    translations: {
+      korean: "왕좌의 게임",
+      polish: "Gra o tron",
+      portuguese: "A Guerra dos Tronos",
+      spanish: "Juego de tronos",
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.44,
+        ratingsCount: 2295233,
+        reviewsCount: 59058,
+      },
+      librarything: {
+        rating: 4.36,
+        ratingsCount: 38358,
+        reviewsCount: 1095,
+      },
+    },
+  },
+];
+
+function getBooks() {
+  return data;
+}
+
+function getBook(id) {
+  return data.find((d) => d.id === id);
+}
+
+const book = getBook(2);
+book;
+
+// const title = book.title;
+// const author = book.author;
+// author;
+
+const {
+  title,
+  author,
+  pages,
+  publicationDate,
+  reviews,
+  genres,
+  hasMovieAdaptation,
+} = book;
+
+console.log(author, title, genres);
+
+// const primaryGenre = genres[0];
+// const secondaryGenre = genres[1];
+
+// modern way: called destructuring, meaning its taking the data from the array then assigning it to the genre index to be called.
+// spread looks like this ...addAnyName which allows to include everything else from the array that hasn't been included.
+const [primaryGenre, secondaryGenre, ...allGenres] = genres;
+console.log(primaryGenre, secondaryGenre, allGenres);
+
+const newGenres = [...genres, "epic fantasy"];
+newGenres;
+
+const updatedBook = {
+  ...book,
+  // Adding a new property
+  moviePublicationDate: "2001-12-19",
+  // Overwritting an existing property
+  pages: 1210,
+};
+updatedBook;
+
+////////////////////////BREAKING DOWN DIFFERENT FORMS OF ARRAY FUNCTIONS AS SAMPLES////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// arraw functions declarative                                                                     /
+// function getYear(str) {                                                                         /
+//   return str.split("-")[0];                                                                     /
+// }                                                                                               /
+
+// console.log(getYear(publicationDate));
+
+// prettier extension wraps the str function to prevent errs and reminder                          /
+// developer can add multiple property. Based on those, we can return something                    /
+// (str, a, b) => str.split("-")[0];                                                               /
+// const getYear = (str) => str.split("-")[0]; // function expression                              /
+
+// const getYear = (str) => {                                                                      /
+//   return str.split("-")[0]; // more then one line require keyword return                        /
+// };                                                                                              /
+// //////////////////////////////////////////////////////////////////////////////////////////////////
+
+const getYear = (str) => str.split("-")[0];
+console.log(getYear(publicationDate));
+
+// template literal
+const summary = `${title}, a ${pages}-page long book was written by ${author} and published on ${
+  // It takes the array value then it splits it based on the value passed on to be split, resulting in an array, then we take first index at 0
+  publicationDate.split("-")[0]
+  // publicationDate
+}. The book has ${hasMovieAdaptation ? "" : "not"} been adapted as a movie`;
+summary;
+
+// Ternaries
+const pagesRange = pages > 1000 ? "over a thousand" : "less than 1000";
+pagesRange;
+console.log(`The book has ${pagesRange} pages`);
+
+// Truethy short circutting
+console.log(true && "some string");
+console.log(false && "some string");
+console.log(hasMovieAdaptation && "This book has a movie");
+
+// falsy: 0, '', null, underfined,
+console.log("Jonas" && "Some string");
+console.log(0 && "Some string");
+
+//nolesh koalesson operator, works simimla to OR operator.
+console.log(book.translations.spanish.split("anillos")[0]);
